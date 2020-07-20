5b4aae578a41350fa5e1e74760bd9ab83b421961
小峰 <dwfn3398@corp.netease.com>
2020-07-20 21:58:35 +0800

    Remove build

--- server/build/config.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const isProduction = process.env.ENV === 'PRODUCTION';
-exports.default = {
-    port: 4000,
-    isProduction
-};
-//# sourceMappingURL=config.js.map
--- server/build/middlewares/index.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-// export { default as LogMiddleware } from './LogMiddleware'
-var ValidateMiddleware_1 = require("./ValidateMiddleware");
-Object.defineProperty(exports, "ValidateMiddleware", { enumerable: true, get: function () { return ValidateMiddleware_1.default; } });
-//# sourceMappingURL=index.js.map
